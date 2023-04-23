e47cfa6b5c3584bf17e7406fc652f08a3c114093
AnuragBetageri <126987627+AnuragBetageri@users.noreply.github.com>
2023-04-23 18:41:02 +0530

    still missing welcome page

--- mini/src/components/js/endcredits.js
+++ mini/src/components/js/endcredits.js
@@ -1,45 +1,79 @@
-import "../css/endcredits.css"
+import "../css/endcredits.css";
 
-import logo from "../image/logo.png"
+import logo from "../image/logo.png";
 
-function endcredits()
-{
-    return(
+function endcredits() {
+  return (
     <div className="maindiv">
-        <div className="A">
-            <div className="A1">
-                <img className="logopng" src={logo} alt=""></img>
-            </div>  
-            <div className="A2">          
-                <p className="rights">@ 2023 Around AI | All rights reserved</p>
-            </div>
+      <div className="A">
+        <div className="A1">
+          <img className="logopng" src={logo} alt=""></img>
         </div>
+        <div className="A2">
+          <p className="rights">@ 2023 Around AI | All rights reserved</p>
+        </div>
+      </div>
 
-        <div className="B">
-           <div className="B1"> 
-                <h2>Information</h2>
-            </div>
-                <div className="B2">
-                    <h4 ><a className="ainfo" href="">FAQ</a></h4>
-                    <h4 ><a className="ainfo" href="">Terms Of Use</a></h4>
-                    <h4 ><a className="ainfo" href="">Privacy Policy</a></h4>
-                    <h4 ><a className="ainfo" href="">Content Policy</a></h4>
-            </div>
+      <div className="B">
+        <div className="B1">
+          <h2>Information</h2>
         </div>
+        <div className="B2">
+          <h4>
+            <a className="ainfo" href="">
+              FAQ
+            </a>
+          </h4>
+          <h4>
+            <a className="ainfo" href="">
+              Terms Of Use
+            </a>
+          </h4>
+          <h4>
+            <a className="ainfo" href="">
+              Privacy Policy
+            </a>
+          </h4>
+          <h4>
+            <a className="ainfo" href="">
+              Content Policy
+            </a>
+          </h4>
+        </div>
+      </div>
 
-        <div className="C">
-            <div className="C1">
-                <h2 >Contact Us</h2>
-            </div>
-            <div className="C2">
-                <h4><span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16" id="IconChangeColor"> <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" id="mainIconPathAttribute" fill="#ffffff"></path> </svg>
-                </span>
-                    <a className="ainfo" href="abhibanur14@gmail.com">support@AroundAI</a></h4>
-            </div>
+      <div className="C">
+        <div className="C1">
+          <h2>Contact Us</h2>
         </div>
+        <div className="C2">
+          <h4>
+            <a className="ainfo" href="abhibanur14@gmail.com">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  class="bi bi-envelope"
+                  viewBox="0 0 16 16"
+                  id="IconChangeColor"
+                >
+                  {" "}
+                  <path
+                    d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"
+                    id="mainIconPathAttribute"
+                    fill="#ffffff"
+                  ></path>{" "}
+                </svg>
+              </div>
+              support@AroundAI
+            </a>
+          </h4>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
 
 export default endcredits;
--- mini/src/components/js/usecases.js
+++ mini/src/components/js/usecases.js
@@ -9,28 +9,27 @@ function usecases() {
   setInterval(usecasestimer4, 12000);
   function usecasestimer4() {
     setTimeout(() => {
-      focused("uh1" , "uimg1" ,"rgb(12,24,36)");
+      focused("uh1", "uimg1", "rgb(12,24,36)");
     }, 0);
 
     setTimeout(() => {
-      focused("uh2","uimg2","rgb(17,252,250)");
+      focused("uh2", "uimg2", "rgb(17,252,250)");
     }, 3000);
     setTimeout(() => {
-      focused("uh3","uimg3", "rgb(72,22,111)");
+      focused("uh3", "uimg3", "rgb(72,22,111)");
     }, 6000);
 
     setTimeout(() => {
-      focused("uh4","uimg4","rgb(16,58,82)");
+      focused("uh4", "uimg4", "rgb(16,58,82)");
     }, 9000);
 
-    function focused(uh , ui , backcolor) {
+    function focused(uh, ui, backcolor) {
       const elements = document.getElementsByClassName(`${uh}`);
       const image = document.getElementsByClassName(`${ui}`);
       const back = document.getElementsByClassName("usecases_sec");
 
-      
-      console.log(uh); 
-      for (let i = 0; i < elements.length; i++) { 
+      console.log(uh);
+      for (let i = 0; i < elements.length; i++) {
         elements[i].classList.toggle("isfocused");
         image[i].classList.toggle("isfocused_img");
         back[i].style.backgroundColor = backcolor;
@@ -44,27 +43,29 @@ function usecases() {
 
   return (
     <section className="usecases_sec">
-      <div className="usecases_body">
-        <header>
-          <h1 className="usecases_h1">Perfect Your Next Work</h1>
-          <span className="use_span">Endless Use Cases</span>
-        </header>
-        <div className="usecases_content">
-          <h2 className={`usecase_h2 uh1 `}>Education ⟶</h2>
-          <h2 className={`usecase_h2 uh2 `}>Advertisement and marketing ⟶</h2>
-          <h2 className={`usecase_h2 uh3 `}>dasfddfd ⟶</h2>
-          <h2 className={`usecase_h2 uh4 `}>affasfasf ⟶</h2>
+      <div className="usecases_content_body">
+        <div className="usecases_body">
+          <header>
+            <h1 className="usecases_h1">Perfect Your Next Work</h1>
+            <span className="use_span">Endless Use Cases</span>
+          </header>
+          <div className="usecases_content">
+            <h2 className={`usecase_h2 uh1 `}>Education ⟶</h2>
+            <h2 className={`usecase_h2 uh2 `}>Advertisement and marketing ⟶</h2>
+            <h2 className={`usecase_h2 uh3 `}>dasfddfd ⟶</h2>
+            <h2 className={`usecase_h2 uh4 `}>affasfasf ⟶</h2>
+          </div>
         </div>
-      </div>
-      <div className="usecases_img_block">
-        <img className="usecase_image uimg1 " src={img1}></img>
-        <img className="usecase_image uimg2" src={img2}></img>
-        <img className="usecase_image uimg3 " src={img3}></img>
-        <img className="usecase_image uimg4 " src={img4}></img>
-        <img className="usecase_image uimg1 " src={img1}></img>
-        <img className="usecase_image uimg2" src={img2}></img>
-        <img className="usecase_image uimg3 " src={img3}></img>
-        <img className="usecase_image uimg4 " src={img4}></img>
+        <div className="usecases_img_block">
+          <img className="usecase_image uimg1 " src={img1}></img>
+          <img className="usecase_image uimg2" src={img2}></img>
+          <img className="usecase_image uimg3 " src={img3}></img>
+          <img className="usecase_image uimg4 " src={img4}></img>
+          <img className="usecase_image uimg1 " src={img1}></img>
+          <img className="usecase_image uimg2" src={img2}></img>
+          <img className="usecase_image uimg3 " src={img3}></img>
+          <img className="usecase_image uimg4 " src={img4}></img>
+        </div>
       </div>
     </section>
   );
