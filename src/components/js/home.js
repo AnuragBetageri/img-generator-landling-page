9262c6f76747c8e9f5bdf881efc325f1ff039f1e
AnuragBetageri <126987627+AnuragBetageri@users.noreply.github.com>
2023-05-09 19:19:42 +0530

    gg

--- src/components/js/home.js
+++ src/components/js/home.js
@@ -9,11 +9,19 @@ function home()
         <div className="home_body">
                 <div className="HomeNavBar">
                     <img src={logo} className="home_logo"></img>
+
+                    <button>Create</button>
+                </div>
+
+                <h1>
+                    Around Ai
+                </h1>
+                <div>
+                    An Ai based Image genration tool 
                 </div>
 
 
-            <svg class="waves" xmlns="http://www.w3.org/2000/svg" xlinkHref="http://www.w3.org/1999/xlink"
-viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+            <svg class="waves" xmlns="http://www.w3.org/2000/svg" xlinkHref="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
 <defs>
 <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
 </defs>
