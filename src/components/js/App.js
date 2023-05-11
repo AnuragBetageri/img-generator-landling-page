16911b2c23b3dc0a8dfabf478d6245b68fc86218
AnuragBetageri <126987627+AnuragBetageri@users.noreply.github.com>
2023-05-12 00:34:45 +0530

    added logo and title

--- src/components/js/App.js
+++ src/components/js/App.js
@@ -1,23 +1,25 @@
-
-import '../css/App.css';
-import Collection from './collection';
-import Getstarted from './getstarted';
-import Usecases from './usecases';
-import Endcredits from './endcredits';
-import Keyfeatures from './key_features';
-import Home from './home';
-
+import "../css/App.css";
+import Collection from "./collection";
+import Getstarted from "./getstarted";
+import Usecases from "./usecases";
+import Endcredits from "./endcredits";
+import Keyfeatures from "./key_features";
+import Home from "./home";
+import {Helmet} from "react-helmet";
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Around AI</title>
+        <meta name="description" content="Helmet application" />
+      </Helmet>
       <Home></Home>
       <Keyfeatures></Keyfeatures>
       <Usecases></Usecases>
       <Collection></Collection>
       <Getstarted></Getstarted>
       <Endcredits></Endcredits>
-    
-      
     </div>
   );
 }
